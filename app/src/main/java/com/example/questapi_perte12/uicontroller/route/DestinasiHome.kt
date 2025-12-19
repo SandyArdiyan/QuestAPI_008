1e2e52a5331f042f2b8d684bda72f8b073653cc0
SandyArdiyan <sanardiyan881@gmail.com>
2025-12-20 02:30:39 +0700

    Menambahkan DestanasiHome.kt

---
+++ app/src/main/java/com/example/questapi_perte12/uicontroller/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.questapi_perte12.uicontroller.route
+
+import com.example.questapi_perte12.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}

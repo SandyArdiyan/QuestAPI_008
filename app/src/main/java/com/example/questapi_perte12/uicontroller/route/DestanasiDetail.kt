4d7d7410a6c6f83b5952b92d065c346863b3b215
SandyArdiyan <sanardiyan881@gmail.com>
2025-12-26 16:35:30 +0700

    Menambahkan DestinasiDetail

--- app/src/main/java/com/example/questapi_perte12/uicontroller/route/DestanasiDetail.kt
+++ app/src/main/java/com/example/questapi_perte12/uicontroller/route/DestanasiDetail.kt
@@ -1,2 +1,8 @@
 package com.example.questapi_perte12.uicontroller.route
 
+object DestanasiDetail : DestinasiNavigasi {
+    override val route = "detail_siswa"
+    override val titleRes = R.string.detail_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "&route/{$itemIdArg}"
+}

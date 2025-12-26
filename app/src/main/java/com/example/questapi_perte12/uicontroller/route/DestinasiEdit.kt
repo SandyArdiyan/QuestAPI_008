6970ca541170ef605c5cf38e800140397f04a241
SandyArdiyan <sanardiyan881@gmail.com>
2025-12-26 22:50:00 +0700

    Menambahkan DestinasiEdit.kt

--- app/src/main/java/com/example/questapi_perte12/uicontroller/route/DestinasiEdit.kt
+++ app/src/main/java/com/example/questapi_perte12/uicontroller/route/DestinasiEdit.kt
@@ -1,8 +1,12 @@
 package com.example.questapi_perte12.uicontroller.route
 
+import com.example.questapi_perte12.R
+
 object DestanasiEdit : DestinasiNavigasi {
     override val route = "item_edit"
     override val titleRes = R.string.edit_siswa
     const val itemIdArg = "idSiswa"
-    val routeWithArgs = "&route/{$itemIdArg}"
+
+    // PERBAIKAN: Gunakan $route
+    val routeWithArgs = "$route/{$itemIdArg}"
 }

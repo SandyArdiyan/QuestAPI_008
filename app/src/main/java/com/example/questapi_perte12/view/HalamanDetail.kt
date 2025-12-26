9279bfe095bd77ac3d4ef849cd3bea7506b5a94a
SandyArdiyan <sanardiyan881@gmail.com>
2025-12-26 23:47:54 +0700

    Menampilkan data siswa (berdasarkan state)

--- app/src/main/java/com/example/questapi_perte12/view/HalamanDetail.kt
+++ app/src/main/java/com/example/questapi_perte12/view/HalamanDetail.kt
@@ -1,2 +1,95 @@
 package com.example.questapi_perte12.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.questapi_perte12.R
+import com.example.questapi_perte12.modeldata.DataSiswa
+import com.example.questapi_perte12.uicontroller.route.DestanasiDetail
+import com.example.questapi_perte12.viewmodel.DetailUiState
+import com.example.questapi_perte12.viewmodel.DetailViewModel
+import com.example.questapi_perte12.viewmodel.provider.PenyediaViewModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailScreen(
+    navigateBack: () -> Unit,
+    navigateToEdit: (Int) -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: DetailViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            SiswaTopAppBar(
+                title = stringResource(DestanasiDetail.titleRes),
+                canNavigateBack = true,
+                navigateUp = navigateBack,
+                scrollBehavior = scrollBehavior
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {
+                    val state = viewModel.detailUiState
+                    if (state is DetailUiState.Success) {
+                        navigateToEdit(state.siswa.id)
+                    }
+                },
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_large))
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    contentDescription = stringResource(R.string.edit_siswa)
+                )
+            }
+        },
+    ) { innerPadding ->
+        DetailStatus(
+            detailUiState = viewModel.detailUiState,
+            retryAction = { viewModel.getSiswaById() },
+            modifier = Modifier.padding(innerPadding),
+            onDeleteClick = {
+                viewModel.deleteSiswa()
+                navigateBack()
+            }
+        )
+    }
+}
+
+}

4aaa0858928fca5323b4c9c8c34708de9e262677
SandyArdiyan <sanardiyan881@gmail.com>
2025-12-26 23:04:00 +0700

    memisahkan dengan jelas antara proses loading, proses sukses (ada data), dan proses gagal.

--- app/src/main/java/com/example/questapi_perte12/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/questapi_perte12/viewmodel/DetailViewModel.kt
@@ -1,2 +1,19 @@
 package com.example.questapi_perte12.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.questapi_perte12.modeldata.DataSiswa
+import com.example.questapi_perte12.repositori.RepositoryDataSiswa
+import com.example.questapi_perte12.uicontroller.route.DestanasiDetail
+import kotlinx.coroutines.launch
+
+sealed interface DetailUiState {
+    data class Success(val siswa: DataSiswa) : DetailUiState
+    object Error : DetailUiState
+    object Loading : DetailUiState
+}
+

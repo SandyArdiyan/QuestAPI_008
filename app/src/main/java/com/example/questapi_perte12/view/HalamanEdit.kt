b75fbb6734c453d85dfbae780772861798a52e56
SandyArdiyan <sanardiyan881@gmail.com>
2025-12-26 23:38:23 +0700

    menambahkan navigasi

--- app/src/main/java/com/example/questapi_perte12/view/HalamanEdit.kt
+++ app/src/main/java/com/example/questapi_perte12/view/HalamanEdit.kt
@@ -1,2 +1,28 @@
 package com.example.questapi_perte12.view
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.questapi_perte12.uicontroller.route.DestanasiEdit
+import com.example.questapi_perte12.viewmodel.EditViewModel
+import com.example.questapi_perte12.viewmodel.provider.PenyediaViewModel
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EditScreen(
+    navigateBack: () -> Unit,
+    onNavigateUp: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EditViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+

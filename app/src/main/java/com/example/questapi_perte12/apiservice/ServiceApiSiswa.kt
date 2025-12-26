b3cb9ce380e298f6a859c6f6c0d722a218c43023
SandyArdiyan <sanardiyan881@gmail.com>
2025-12-26 17:07:33 +0700

    Menambahkan PUT dan DELETE

--- app/src/main/java/com/example/questapi_perte12/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/questapi_perte12/apiservice/ServiceApiSiswa.kt
@@ -1,6 +1,7 @@
 package com.example.questapi_perte12.apiservice
 
 import com.example.questapi_perte12.modeldata.DataSiswa
+import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.GET
@@ -12,6 +13,15 @@ interface ServiceApiSiswa {
     @GET("bacaTeman.php")
     suspend fun getSiswa(): List<DataSiswa>
 
+    @GET("bacaTeman.php")
+    suspend fun getSiswaById(@Query("id") id: Int): DataSiswa
+
     @POST("insertTM.php")
-    suspend fun postSiswa(@Body dataSiswa: DataSiswa): retrofit2.Response<Void>
+    suspend fun postSiswa(@Body dataSiswa: DataSiswa): Response<Void>
+
+    @PUT("editTeman.php")
+    suspend fun updateSiswa(@Query("id") id: Int, @Body dataSiswa: DataSiswa): Response<Void>
+
+    @DELETE("deleteTeman.php")
+    suspend fun deleteSiswa(@Query("id") id: Int): Response<Void>
 }
